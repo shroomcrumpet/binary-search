4314b3e6dbbc8ed93d23871a98d120aad7880c68
shroomcrumpet <kencheng3@gmail.com>
2018-11-09 13:34:39 +0800

    Fixed issues with splice vs slice; added while loop code option

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const nameList = [
     'Zoro'
 ];
 
-
+// Using recursion
 function binarySearch(list, name) {
 
     let indexHalf = Math.floor(list.length / 2);
@@ -24,10 +24,12 @@ function binarySearch(list, name) {
         console.log("true");
         return true;
     } else if ( list[indexHalf] > name ) {
-        list = list.splice(0, indexHalf);
+        // list = list.splice(0, indexHalf);
+        list = list.slice(0, indexHalf);
         console.log("front", list)
     } else if ( list[indexHalf] < name ) {
-        list = list.splice(indexHalf + 1, list.length - indexHalf - 1);
+        // list = list.splice(indexHalf + 1, list.length - indexHalf - 1);
+        list = list.slice(indexHalf + 1);
         console.log("back", list)
     };
 
@@ -35,6 +37,48 @@ function binarySearch(list, name) {
 };
 
 
+// Using while loop
+// const binarySearch = (list, name) => {
 
-// binarySearch(nameList, 'Daisy');//=> true
-// binarySearch(nameList, 'Bruce'); //=> false
+//     console.log("sorting list: ", list);
+
+//     while (true) {
+
+//         let indexHalf = Math.floor(list.length / 2);
+
+//         if (list.length === 1 && list[0] != name) {
+//             console.log("false");
+//             return false;
+//         } else if (list[indexHalf] === name) {
+//             console.log("true");
+//             return true;
+//         } else if ( list[indexHalf] > name ) {
+//             // list = list.splice(0, indexHalf);
+//             list = list.slice(0, indexHalf)
+//             console.log("front", list)
+//         } else if ( list[indexHalf] < name ) {
+//             // list = list.splice(indexHalf + 1, list.length - indexHalf - 1);
+//             list = list.slice(indexHalf + 1);
+//             console.log("back", list)
+//         };
+//     };
+// };
+
+
+
+binarySearch(nameList, 'Daisy');//=> true
+binarySearch(nameList, 'Aaron');//=> true
+binarySearch(nameList, 'Bruce'); //=> false
+
+
+
+
+
+
+
+
+
+
+
+
+
